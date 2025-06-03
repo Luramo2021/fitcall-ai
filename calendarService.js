9f3bdd96215aef36c003fa47f6e4a80058577ce1
Luramo2021 <kinkonluramo@gmail.com>
2025-06-03 10:55:38 +0200

    Update calendarService.js

--- calendarService.js
+++ calendarService.js
@@ -3,10 +3,9 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(
   console.log("🔗 SUPABASE_URL:", process.env.SUPABASE_URL);
   console.log("🔑 SUPABASE_ANON_KEY starts with:", process.env.SUPABASE_ANON_KEY?.slice(0, 10));
-
+);
   //process.env.SUPABASE_URL,
  // process.env.SUPABASE_ANON_KEY
-);
 
 async function getAvailableSlots() {
   const { data, error } = await supabase
